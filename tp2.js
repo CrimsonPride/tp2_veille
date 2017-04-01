df1507a776a5bd194204769070b60d1f8c775995
Patrick Lauzon <patrick.lauzon94@hotmail.com>
2017-03-31 23:06:09 -0400

    Arrangement des fonctions trier plus completion de modifier

--- tp2.js
+++ tp2.js
@@ -21,36 +21,36 @@ MongoClient.connect('mongodb://127.0.0.1:27017/carnet_6', (err, database) => {
 
 
 app.get('/', function (req, res) {
-    console.log("dans fonction");
     var cursor = db.collection('carnet_6').find().toArray(function(err, resultat){
        if (err) return console.log(err)
     // renders index.ejs
     // affiche le contenu de la BD
-    console.log("dans db");
     res.render('index.ejs', {carnet: resultat})
 
     }) 
     
 })
 
-
-app.get('/Ajouter', function (req, res) {
-    //console.log("je suis ajouter");
-    //Génère un nombre aléatoire pour la capitale
+app.get('/Afficher',  (req, res) => {
    
-    //Ajoute une province au à la collection Provinces
-    db.collection('carnet_6').insert(
-        {"nom": "",
-         "prenom": "",
-         "telephone":""}
-    )
-    //Renvoi à Collection
-    res.redirect('/')
+   console.log('la route  get / = ' + req.url)
+   res.sendFile(__dirname + "/public/html/forme.html")
 })
 
-app.get('/Detruire/:_id',  (req, res) => {
 
-    db.collection('carnet_6').findOneAndDelete({nom:"Lauzon"}, (err, resultat) => {
+app.post('/Ajouter',  (req, res) => {
+  db.collection('carnet_6').save(req.body, (err, result) => {
+      if (err) return console.log(err)
+      console.log('sauvegarder dans la BD')
+      res.redirect('/')
+    })
+})
+
+
+app.get('/Detruire/:_id',  (req, res) => {
+     var id = req.params._id
+    console.log(id)
+    db.collection('carnet_6').findOneAndDelete({"_id": ObjectID(req.params._id)}, (err, resultat) => {
 
     if(err) return res.send(500,err)
     var cursor = db.collection('carnet_6').find().toArray(function(err, resultat){
@@ -62,9 +62,45 @@ app.get('/Detruire/:_id',  (req, res) => {
   }) 
 })
 
-app.post('/Modifier',  (req, res) => {
 
-    db.collection('carnet_6').update({_id:req.body._id},{$set:{'nom':req.body.nom, 'prenom':req.body.prenom, 'telephone':req.body.telephone}}, (err, resultat) => {
+app.get('/Yolo',  (req, res) => {
+
+     var cursor = db.collection('carnet_6').find().toArray(function(err, resultat){
+       if (err) return console.log(err)
+    // renders index.ejs
+    // affiche le contenu de la base de donnée
+    console.log(req.url.slice(-1));
+    res.render('index1.ejs', {carnet: resultat, id:req.url.slice(-1)})
+    }) 
+})
+
+app.get('/Modifier/:_id',  (req, res) => {
+   var id = req.params._id
+    console.log();
+    db.collection('carnet_6').findAndModify({
+    query: {_id:ObjectID(req.body._id)},
+    update: {$set: {nom: req.body.nom, prenom: req.body.prenom, telephone: req.body.telephone}},
+    new: true
+    }, (err, resultat) => {
+
+    if(err) return res.send(500,err)
+    var cursor = db.collection('carnet_6').find().toArray(function(err, resultat){
+       if (err) return console.log(err)
+    // renders index.ejs
+    // affiche le contenu de la base de donnée
+    res.redirect('/');
+    }) 
+  }) 
+})
+
+app.get('/Ding',  (req, res) => {
+   var id = req.params._id
+    console.log();
+    db.collection('carnet_6').findAndModify({
+    query: {_id:ObjectID(req.body._id)},
+    update: {$set: {nom: req.body.nom, prenom: req.body.prenom, telephone: req.body.telephone}},
+    new: true
+    }, (err, resultat) => {
 
     if(err) return res.send(500,err)
     var cursor = db.collection('carnet_6').find().toArray(function(err, resultat){
@@ -77,10 +113,17 @@ app.post('/Modifier',  (req, res) => {
 })
 
 app.get('/TNom',  (req, res) => {
-    db.collection('carnet_6').find( { $query: {}, $orderby: { nom: "A" } } )
+    
+    
+    db.collection('carnet_6').find().sort({ nom: -1 }, (err, resultat) => {
+
+    if(err) return res.send(500,err)
+    var cursor = db.collection('carnet_6').find().toArray(function(err, resultat){
+       if (err) return console.log(err)
     // renders index.ejs
     // affiche le contenu de la base de donnée
     res.redirect('/');
-    
-}) 
+    }) 
+  }) 
+})
   
